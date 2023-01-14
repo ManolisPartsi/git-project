0379b1f8a661742418af9565b4a997a4387890e1
Manos. Partsi <tanakarei93@gmail.com>
2023-01-14 15:07:22 +0200

    version 0.2

---
+++ src/model/Teacher.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Teacher {
+    long id;
+    String lastname;
+    String firstname;
+
+    public Teacher(long id, String lastname, String firstname){
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
